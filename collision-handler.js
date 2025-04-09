f6bae1fbd5f29c638e6f14d8d80f0cf45d42d8a8
Benjamin Valero <bvalero69afec@proton.me>
2025-04-09 13:26:06 +0200

    Starting collision handling

---
+++ collision-handler.js
@@ -0,0 +1,24 @@
+/*
+Will use:
+player.x
+player.y
+player.width
+player.height
+player.velx (the function can update the value if collision occurs)
+player.vely (the function can update the value if collision occurs)
+borders.left.x
+borders.right.x
+borders.bottom.y
+borders.top.y
+floor.x
+floor.y
+floor.width
+floor.height
+obstacles[i].x
+obstacles[i].y
+obstacles[i].width
+obstacles[i].height
+*/
+function handleCollision(player, borders, floor, obstacles) {
+
+}
