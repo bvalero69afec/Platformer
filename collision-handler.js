46b2b90ac92f41691ad011fd2994744c1084d420
Benjamin Valero <bvalero69afec@proton.me>
2025-04-10 00:21:33 +0200

    Rudimentary collision handler

--- collision-handler.js
+++ collision-handler.js
@@ -1,24 +1,59 @@
-/*
-Will use:
-player.x
-player.y
-player.width
-player.height
-player.velx (the function can update the value if collision occurs)
-player.vely (the function can update the value if collision occurs)
-borders.left.x
-borders.right.x
-borders.bottom.y
-borders.top.y
-floor.x
-floor.y
-floor.width
-floor.height
-obstacles[i].x
-obstacles[i].y
-obstacles[i].width
-obstacles[i].height
-*/
-function handleCollision(player, borders, floor, obstacles) {
+const GAMESTATE_IN_PROGRESS = 0;
+const GAMESTATE_LOSE = 1;
+const GAMESTATE_WIN = 2;
 
+function handleCollisionFloor(player, groundY){
+  if (player.velY < 0) {
+    if (player.y + player.velY < groundY) {
+      player.velY = groundY - player.y;
+    }
+  }
+}
+
+function handleCollisionLeftAndTopBorders(player, leftBorderX, topBorderY) {
+  if (player.velX < 0) {
+    if (player.x + player.velX < leftBorderX) {
+      player.velX = leftBorderX - player.x;
+    }
+  }
+
+  if (player.velY > 0) {
+    if (player.y + player.height + player.velY > topBorderY) {
+      player.velY = topBorderY - (player.y + player.height);
+    }
+  }
+}
+
+function isRectangleCollidingRectangle(rect1, rect2) {
+  return (
+    rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y
+  );
+}
+
+function checkWin(player, rightBorderX) {
+  if (player.velX > 0) {
+    if (player.x + player.width + player.velX > rightBorderX) {
+      return GAMESTATE_WIN;
+    }
+  }
+
+  return GAMESTATE_IN_PROGRESS;
+}
+
+function handleCollision(player, groundY, leftBorderX, topBorderY, obstacles, rightBorderX) {
+  handleCollisionFloor(player, groundY);
+  
+  handleCollisionLeftAndTopBorders(player, leftBorderX, topBorderY);
+
+  let playerNewPosRect = { x: player.x + player.velX, y: player.y + player.velY, width: player.width, height: player.height};
+  for (const obstacle of obstacles) {
+      if (isRectangleCollidingRectangle(playerNewPosRect, obstacle)) {
+          return GAMESTATE_LOSE;
+      }
+  }
+
+  return checkWin(player, rightBorderX);
 }
