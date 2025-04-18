67b9334ed9afd6e9394ede6045830419523d3a71
Benjamin Valero <bvalero69afec@proton.me>
2025-04-18 10:05:17 +0200

    Typo

--- basegame/js/score.js
+++ basegame/js/score.js
@@ -3,7 +3,7 @@ function calculatePlayerScore(world, obstacleAreaX, obstacleAreaWidth) {
   const player = world.player;
 
   const playerXClampedAtMin = Math.max(player.x, obstacleAreaX);
-  const distanceToObstacleAreaStart = playerXClampedAtMin - obstacleAreaX; 
+  const distanceToObstacleAreaStart = playerXClampedAtMin - obstacleAreaX;
   const distanceClampedToObstacleArea = Math.min(distanceToObstacleAreaStart, obstacleAreaWidth);
   const flooredClampedDistance = Math.floor(distanceClampedToObstacleArea);
   return flooredClampedDistance;
